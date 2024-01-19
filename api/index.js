a1c8ec68790a64653fe3caece64d583acbbdd208
allelbhagya <allel.bhagya@gmail.com>
2024-01-19 12:53:47 +0530

    token

--- api/index.js
+++ api/index.js
@@ -58,27 +58,24 @@ app.post('/login', async (req,res) => {
 
   app.get('/profile', async (req, res) => {
     try {
-        const { token } = req.cookies;
+      const { token } = req.cookies;
 
-        if (!token) {
-            return res.status(401).json({ error: 'Unauthorized - No token found' });
-        }
+      console.log('Received token:', token);  // Add this line for debugging
 
-        console.log('Token in /profile request:', token); // Add this line for debugging
+      if (!token) {
+        return res.status(401).json({ error: 'Unauthorized - No token found' });
+      }
 
-        const info = await jwt.verify(token, secret);
+      const info = await jwt.verify(token, secret);
+      console.log('User profile info:', info); // Add this line for debugging
 
-        console.log('User profile info:', info);
-
-        res.json(info);
+      res.json(info);
     } catch (error) {
-        console.error('Error during profile:', error);
-        res.status(401).json({ error: 'Unauthorized - Invalid token' });
+      console.error('Error during profile:', error);
+      res.status(401).json({ error: 'Unauthorized - Invalid token' });
     }
 });
 
-  
-  
 
 app.post('/logout', (req,res)=>{
     res.cookie('token', '').json('ok');
