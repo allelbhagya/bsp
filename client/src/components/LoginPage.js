8a69520f146534c5543001a2b38bcdf940f08951
allelbhagya <allel.bhagya@gmail.com>
2024-01-19 10:41:49 +0530

    domains

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -9,7 +9,7 @@ export default function LoginPage(){
     const {setUserInfo} = useContext(UserContext);
     async function login(ev){
         ev.preventDefault();
-        const response = await fetch('https://t-bsp-api.vercel.app//login',{
+        const response = await fetch('https://t-bsp-api.vercel.app/login',{
             method:'POST',
             body:JSON.stringify({username,password}),
             headers:{'Content-Type':'application/json'},
